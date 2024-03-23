a2b466564a808f21a8ed8fa3f5dc7bb14c6774c6
mynameisserzheo@gmail.com <mynameisserzheo@gmail.com>
2024-03-23 14:08:24 +0200

    Remove unused

--- Lecture/P1/Program.cs
+++
@@ -1,7 +0,0 @@
-using P1;
-
-CreateHostBuilder(args).Build().Run();
-
-static IHostBuilder CreateHostBuilder(string[] args) =>
-Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
